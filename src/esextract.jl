ef8b1dad49b506c89de5d94300f87e7e9843b5ca
Jamie <jamierayhutchings@gmail.com>
2021-04-18 09:09:08 +1200

    first commit

---
+++ src/esextract.jl
@@ -0,0 +1,125 @@
+module esextract
+export get_query_dataframe, get_query_json, write_dataframe, read_dataframe
+
+using PyCall
+using DataFrames
+using CSV
+
+"""
+Converts from a Python Pandas DataFrame to a Julia DataFrame
+...
+# Arguments
+- `p_df::Pandas DataFrame`: a pandas python DataFrame
+# Returns 
+- `DataFrame`: julia DataFrame
+...
+"""
+function p_df_to_j_df(p_df)
+    println("Converting from python DataFrame to julia DataFrame")
+    cols = p_df[:columns]
+    j_df = DataFrame(Any[collect(values(p_df[c])) for c in cols], map(Symbol, cols))
+    return j_df
+end
+"""
+Takes elasticsearch query parameters and returns result as DataFrame
+...
+# Arguments
+- `index::String`: the elasticsearch index you want to query
+- `paging_id_field::String` (str): the id field to page on
+- `paging_time_field::String`: the date/time field to page on
+- `return_fields::Array`: the fields you want returned from the query
+- `fields_to_search::Array`: the fields you want to search for your query string in
+- `search_string::String`: the terms you want to search for in the search fields
+- `field_to_exist::String`: supplied field will be used as an extra check to 
+    only return documents where this field isn't null
+- `date_field::String`: supplied field will be used to search by a custom date field
+    use in conjunction with start_date and end_date args
+- `start_date::String`: the first date you want to return documents from in format
+    yyyy-mm-dd
+- `end_date::String`: the last date you want to return documents from in format
+    yyyy-mm-dd, can also be set to 'now' to use current date
+- `is_match_all::Number`: this overrides search terms and exist terms and returns
+    all documents between start and end dates if specified
+# Returns 
+- `DataFrame`: julia DataFrame
+...
+"""
+function get_query_dataframe(;index=nothing, paging_id_field=nothing, paging_time_field=nothing, return_fields=[], fields_to_search=[], 
+    search_string=nothing, field_to_exist=nothing, date_field=nothing, start_date=nothing, end_date=nothing, is_match_all=false)
+    
+    es = pyimport("esextract")
+
+    p_df = es.query_to_dataframe(index, paging_id_field, paging_time_field, return_fields, 
+        fields_to_search, search_string, field_to_exist, date_field, start_date, end_date, is_match_all)
+    
+    df = p_df_to_j_df(p_df)
+
+    return df
+end
+
+"""
+Takes elasticsearch query parameters and returns result as a JSON dictionary
+...
+# Arguments
+- `index::String`: the elasticsearch index you want to query
+- `paging_id_field::String` (str): the id field to page on
+- `paging_time_field::String`: the date/time field to page on
+- `return_fields::Array`: the fields you want returned from the query
+- `fields_to_search::Array`: the fields you want to search for your query string in
+- `search_string::String`: the terms you want to search for in the search fields
+- `field_to_exist::String`: supplied field will be used as an extra check to 
+    only return documents where this field isn't null
+- `date_field::String`: supplied field will be used to search by a custom date field
+    use in conjunction with start_date and end_date args
+- `start_date::String`: the first date you want to return documents from in format
+    yyyy-mm-dd
+- `end_date::String`: the last date you want to return documents from in format
+    yyyy-mm-dd, can also be set to 'now' to use current date
+- `is_match_all::Number`: this overrides search terms and exist terms and returns
+    all documents between start and end dates if specified
+# Returns 
+- `Dict`: Json results
+...
+"""
+function get_query_json(;index=nothing, paging_id_field=nothing, paging_time_field=nothing, return_fields=[], fields_to_search=[], 
+    search_string=nothing, field_to_exist=nothing, date_field=nothing, start_date=nothing, end_date=nothing, is_match_all=false)
+
+    es = pyimport("esextract")
+
+    json = es.query_to_json(index, paging_id_field, paging_time_field, return_fields, 
+        fields_to_search, search_string, field_to_exist, date_field, start_date, end_date, is_match_all)
+
+    return json
+end
+
+"""
+Writes a DataFrame to CSV
+...
+# Arguments
+- `path::String`: path to file output
+- `df::DataFrame`: DataFrame to save
+...
+"""
+function write_dataframe(path, df)
+    println("Writing dataframe to csv")
+    CSV.write(path, df)
+end
+
+"""
+Reads a DataFrame in from csv, json and arrow parquet
+...
+# Arguments
+- `path::String`: path to file output
+# Returns
+- `df::DataFrame`: DataFrame from file
+...
+"""
+function read_dataframe(path)
+    println("Reading data into dataframe")
+    es = pyimport("esextract")
+    p_df = es.read_dataframe_from_file(path)
+    df = p_df_to_j_df(p_df)
+    return df
+end
+
+end # module
